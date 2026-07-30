c9c5e52c51405bb041d09f1225e1cb2b3328f239
Florin Butnaru <butnaru.florin@gmail.com>
2026-07-30 19:21:52 +0300

    fix: add turnstileSiteKey to test environment

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   supabaseUrl: 'http://127.0.0.1:54321',
-  supabaseKey: 'sb_publishable_ACJWlzQHlZjBrEguHvfOxg_3BJgxAaH'
+  supabaseKey: 'sb_publishable_ACJWlzQHlZjBrEguHvfOxg_3BJgxAaH',
+  turnstileSiteKey: ''
 };
